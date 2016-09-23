26a63bbc22537f85230c9522c0af863eb10b26ce
mydongistiny <jaysonedson@gmail.com>
2016-09-22 23:00:06 -0700

    addons: Bring in newer Superuser
    Newer version of phh. This one with a Light/Dark theme switch
    
    Also bring in and update the su binary
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config.mk
+++ config.mk
@@ -3,7 +3,8 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 # init file
 PRODUCT_COPY_FILES += \
-    vendor/addons/system/init.superuser.rc:root/init.superuser.rc
+    vendor/addons/superuser/su:root/sbin/su \
+    vendor/addons/superuser/init.superuser.rc:root/init.superuser.rc
 
 # Prebuilts
 PRODUCT_PACKAGES += \
