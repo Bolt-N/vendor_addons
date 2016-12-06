349b2be830e1e53191f248f581af8bc68ed40b5f
winga45 <winga45@gmail.com>
2016-12-06 05:14:28 -0500

    remove superuser

--- config.mk
+++ config.mk
@@ -5,13 +5,7 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.substratum.verified=true \
 
-# init file
-PRODUCT_COPY_FILES += \
-    vendor/addons/superuser/su:root/sbin/su \
-    vendor/addons/superuser/init.superuser.rc:root/init.superuser.rc
-
 # Prebuilts
 PRODUCT_PACKAGES += \
     KernelAdiutor \
-    Substratum \
-    Superuser 
+    Substratum 
